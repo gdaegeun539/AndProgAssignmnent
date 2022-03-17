b060e901aac662748affb0a7a6f04517f3a2e8ae
gdaegeun539 <gdaegeun539@gmail.com>
2022-03-17 11:03:52 +0900

    4-1 나머지 버튼, 입력확인 메소드, 계산통합 메소드 추가

--- app/src/main/java/com/example/project4_1/MainActivity.kt
+++ app/src/main/java/com/example/project4_1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var edit1 : EditText
@@ -13,10 +14,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnSub : Button
     lateinit var btnMul : Button
     lateinit var btnDiv : Button
+    lateinit var btnMod: Button
     lateinit var textResult : TextView
-    lateinit var num1 : String
-    lateinit var num2 : String
-    var result : Int? = null
+    lateinit var notEdit: Toast
+    var result : Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +27,73 @@ class MainActivity : AppCompatActivity() {
         edit1 = findViewById(R.id.Edit1)
         edit2 = findViewById(R.id.Edit2)
         btnAdd = findViewById(R.id.BtnAdd)
+        btnSub = findViewById(R.id.BtnSub)
+        btnMul = findViewById(R.id.BtnMul)
+        btnDiv = findViewById(R.id.BtnDiv)
+        btnMod = findViewById(R.id.BtnMod)
         textResult = findViewById(R.id.TextResult)
+        notEdit = Toast.makeText(this, R.string.not_input, Toast.LENGTH_SHORT)
 
-        btnAdd.setOnTouchListener { _, _ ->
-            num1 = edit1.text.toString()
-            num2 = edit2.text.toString()
-            result = num1.toInt() + num2.toInt()
+        btnAdd.setOnClickListener {
+            result = edit1.text.toString().toDouble() + edit2.text.toString().toDouble()
             textResult.text = "계산 결과: " + result.toString()
-
-            false
         }
+
+        btnSub.setOnClickListener {
+            result = edit1.text.toString().toDouble() - edit2.text.toString().toDouble()
+            textResult.text = "계산 결과 : " + result.toString()
+        }
+
+        btnMul.setOnClickListener {
+            result = edit1.text.toString().toDouble() - edit2.text.toString().toDouble()
+            textResult.text = "계산 결과 : " + result.toString()
+        }
+
+        btnDiv.setOnClickListener {
+            result = edit1.text.toString().toDouble() - edit2.text.toString().toDouble()
+            textResult.text = "계산 결과 : " + result.toString()
+        }
+
+        btnMod.setOnClickListener {
+            if (isInput()) {
+                notEdit.show()
+            } else {
+                doCalc("Mod")
+            }
+        }
+
+    }
+
+    private fun isInput(/* edit1: EditText, edit2: EditText */): Boolean {
+        return edit1.text.toString() == "" || edit2.text.toString() == ""
+    }
+
+    private fun doCalc(stat: String) {
+        // TODO: Not yet impl
+
+        if (edit2.text.toString().toInt() == 0) {
+            Toast.makeText(this, R.string.div_zero, Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        when (stat) {
+            "Add" -> {
+                // TODO: Not yet impl
+            }
+            "Sub" -> {
+                // TODO: Not yet impl
+            }
+            "Mul" -> {
+                // TODO: Not yet impl
+            }
+            "Div" -> {
+                // TODO: Not yet impl
+            }
+            "Mod" -> {
+                result = edit1.text.toString().toDouble() % edit2.text.toString().toDouble()
+                textResult.text = "계산 결과 : " + result.toString()
+            }
+        }
+
     }
 }
