d649a35d40a045497e57b8a64b30340921a706a6
gdaegeun539 <gdaegeun539@gmail.com>
2022-03-17 14:45:28 +0900

    4-2 init

--- project4_2/src/main/java/com/example/project4_2/MainActivity.kt
+++ project4_2/src/main/java/com/example/project4_2/MainActivity.kt
@@ -7,13 +7,14 @@ import android.widget.*
 class MainActivity : AppCompatActivity() {
     lateinit var text1 : TextView
     lateinit var text2 : TextView
-    lateinit var chkAgree : CheckBox
+    lateinit var swAgree : CheckBox
     lateinit var rGroup1 : RadioGroup
-    lateinit var rdoDog : RadioButton
-    lateinit var rdoCat : RadioButton
-    lateinit var rdoRabbit : RadioButton
-    lateinit var btnOK : Button
-    lateinit var imgPet : ImageView
+    lateinit var rdoOreo : RadioButton
+    lateinit var rdoPie : RadioButton
+    lateinit var rdoQueenCake : RadioButton
+    lateinit var btnExit : Button
+    lateinit var imgAndro : ImageView
+    lateinit var btnClear: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,40 +22,61 @@ class MainActivity : AppCompatActivity() {
         title = "2018156029 이대근"
 
         text1 = findViewById(R.id.Text1)
-        chkAgree = findViewById(R.id.ChkAgree)
+        swAgree = findViewById(R.id.ChkAgree)
         text2 = findViewById(R.id.Text2)
         rGroup1 = findViewById(R.id.Rgroup1)
-        rdoDog = findViewById(R.id.RdoDog)
-        rdoCat = findViewById(R.id.RdoCat)
-        rdoRabbit = findViewById(R.id.RdoRabbit)
-        btnOK = findViewById(R.id.BtnOK)
-        imgPet = findViewById(R.id.ImgPet)
+        rdoOreo = findViewById(R.id.RdoOreo)
+        rdoPie = findViewById(R.id.RdoPie)
+        rdoQueenCake = findViewById(R.id.RdoQueenCake)
+        imgAndro = findViewById(R.id.ImgAndroid)
+        btnExit = findViewById(R.id.BtnExit)
+        btnClear = findViewById(R.id.BtnClear)
 
-        // 시작함 체크박스의 체크가 변경되면
-        chkAgree.setOnCheckedChangeListener { compoundButton, b ->
+        // 시작함 스위치의 체크가 변경되면
+        swAgree.setOnCheckedChangeListener { compoundButton, b ->
             // 체크되면 모두 보이도록 설정
-            if (chkAgree.isChecked) {
+            if (swAgree.isChecked) {
                 text2.visibility = android.view.View.VISIBLE
                 rGroup1.visibility = android.view.View.VISIBLE
-                btnOK.visibility = android.view.View.VISIBLE
-                imgPet.visibility = android.view.View.VISIBLE
+                imgAndro.visibility = android.view.View.VISIBLE
+                btnExit.visibility = android.view.View.VISIBLE
+                btnClear.visibility = android.view.View.VISIBLE
             } else {
                 text2.visibility = android.view.View.INVISIBLE
                 rGroup1.visibility = android.view.View.INVISIBLE
-                btnOK.visibility = android.view.View.INVISIBLE
-                imgPet.visibility = android.view.View.INVISIBLE
+                imgAndro.visibility = android.view.View.INVISIBLE
+                btnExit.visibility = android.view.View.INVISIBLE
+                btnClear.visibility = android.view.View.INVISIBLE
             }
         }
 
-        // 선택 완료 버튼을 클릭하면
-        btnOK.setOnClickListener {
-            when (rGroup1.checkedRadioButtonId) {
-                R.id.RdoDog -> imgPet.setImageResource(R.drawable.dog)
-                R.id.RdoCat -> imgPet.setImageResource(R.drawable.cat)
-                R.id.RdoRabbit -> imgPet.setImageResource(R.drawable.rabbit)
-                else -> Toast.makeText(applicationContext, "동물 먼저 선택하세요",
-                    Toast.LENGTH_SHORT).show()
-            }
+        rdoOreo.setOnCheckedChangeListener { compoundButton, b ->
+            TODO("Not yet Impl")
+        }
+
+        rdoPie.setOnCheckedChangeListener { compoundButton, b ->
+            TODO("Not yet Impl")
+        }
+
+        rdoQueenCake.setOnCheckedChangeListener { compoundButton, b ->
+            TODO("Not yet Impl")
+        }
+
+        // 종료 버튼 클릭
+        btnExit.setOnClickListener {
+            TODO("Not yet Impl")
+//            when (rGroup1.checkedRadioButtonId) {
+//                R.id.RdoDog -> imgPet.setImageResource(R.drawable.dog)
+//                R.id.RdoCat -> imgPet.setImageResource(R.drawable.cat)
+//                R.id.RdoRabbit -> imgPet.setImageResource(R.drawable.rabbit)
+//                else -> Toast.makeText(applicationContext, "동물 먼저 선택하세요",
+//                    Toast.LENGTH_SHORT).show()
+//            }
+        }
+        
+        // 처음으로 버튼 클릭
+        btnClear.setOnClickListener {
+            TODO("Not Yet Impl")
         }
     }
 }
