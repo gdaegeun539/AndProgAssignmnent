50d7a4452b855ace54fde10ec41f46501376d066
gdaegeun539 <gdaegeun539@gmail.com>
2022-03-17 15:31:36 +0900

    4-3 init

--- project4_3/src/main/java/com/example/project4_3/MainActivity.kt
+++ project4_3/src/main/java/com/example/project4_3/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.project4_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toastText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toastText = findViewById(R.id.toastText)
+        toastText.inputType = 0
+        toastText.setOnKeyListener { _, _, _ ->
+            Toast.makeText(this, toastText.text.toString(), Toast.LENGTH_SHORT).show()
+            false
+        }
     }
 }
